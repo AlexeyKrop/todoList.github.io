bb1a2203f4007a8aef0e6d22112033dc3a4ec857
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-02 10:59:52 +0300

    add taskReducer.test.ts

--- src/Bll/Reducers/taskReducer.test.ts
+++ src/Bll/Reducers/taskReducer.test.ts
@@ -1,15 +1,15 @@
 import {AllTaskType} from "../../App";
-import {addTaskAC, taskReducer} from "./taskReducer";
+import {addTaskAC, removeTaskAC, taskReducer} from "./taskReducer";
 
 let startState: AllTaskType = {};
 beforeEach(()=>{
   startState = {
-    'todolistId1': [
+    'todoListId1': [
       {id: '1', title: 'CSS', isDone: false},
       {id: '2', title: 'JS', isDone: true},
       {id: '3', title: 'React', isDone: false}
     ],
-    'todolistId2': [
+    'todoListId2': [
       {id: '1', title: 'bread', isDone: false},
       {id: '2', title: 'milk', isDone: true},
       {id: '3', title: 'tea', isDone: false}
@@ -17,11 +17,27 @@ beforeEach(()=>{
   }
 })
 test('check add task', ()=> {
-  const action = addTaskAC('todolistId2','juce')
+  const action = addTaskAC('todoListId2','juce')
   const endState = taskReducer(startState, action)
-  expect(endState!['todolistId1'].length).toBe(3)
-  expect(endState!['todolistId2'].length).toBe(4)
-  expect(endState!['todolistId2'][0].id).toBeDefined()
-  expect(endState!['todolistId2'][0].title).toBe('juce')
-  expect(endState!['todolistId2'][0].isDone).toBe(false)
+  expect(endState['todoListId1'].length).toBe(3)
+  expect(endState['todoListId2'].length).toBe(4)
+  expect(endState['todoListId2'][0].id).toBeDefined()
+  expect(endState['todoListId2'][0].title).toBe('juce')
+  expect(endState['todoListId2'][0].isDone).toBe(false)
+} )
+test('check remove task', ()=> {
+  const action = removeTaskAC('todoListId1','1')
+  const endState = taskReducer(startState, action)
+  expect(endState).toEqual({
+    'todoListId1': [
+
+      {id: '2', title: 'JS', isDone: true},
+      {id: '3', title: 'React', isDone: false}
+    ],
+    'todoListId2': [
+      {id: '1', title: 'bread', isDone: false},
+      {id: '2', title: 'milk', isDone: true},
+      {id: '3', title: 'tea', isDone: false}
+    ]
+  })
 } )
--- src/Bll/Reducers/taskReducer.ts
+++ src/Bll/Reducers/taskReducer.ts
@@ -1,20 +1,27 @@
 import {AllTaskType} from "../../App";
 import {v1} from "uuid";
 
-export const taskReducer = (tasks: AllTaskType, action:TaskACType) => {
+export const taskReducer = (tasks: AllTaskType, action: TaskACType) => {
   switch (action.type) {
     case "ADD_TASK":
       let newTask = {id: v1(), title: action.taskTitle, isDone: false};
       return {...tasks, [action.todoListId]: [newTask, ...tasks[action.todoListId]]}
+    case "REMOVE_TASK":
+      return {...tasks, [action.todoListId]: tasks[action.todoListId].filter(t => t.id !== action.taskId)}
   }
 }
 
 
-
-
-
-
-
-type TaskACType = AddTaskAT
+type TaskACType = AddTaskAT | RemoveTaskAT
 type AddTaskAT = ReturnType<typeof addTaskAC>
-export const addTaskAC = (todoListId: string, title: string) =>({type: 'ADD_TASK',todoListId: todoListId, taskTitle: title})
+type RemoveTaskAT = ReturnType<typeof removeTaskAC>
+export const addTaskAC = (todoListId: string, title: string) => ({
+  type: 'ADD_TASK',
+  todoListId: todoListId,
+  taskTitle: title
+} as const)
+export const removeTaskAC = (todoListId: string, taskId: string) => ({
+  type: 'REMOVE_TASK',
+  todoListId: todoListId,
+  taskId: taskId
+} as const)
