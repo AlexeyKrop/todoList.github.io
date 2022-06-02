43f0f6ed36104ff26288778c454bcecea0c9704f
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-02 09:45:34 +0300

    add change title in Todolist test

--- src/Bll/Reducers/todolistReducer.test.ts
+++ src/Bll/Reducers/todolistReducer.test.ts
@@ -1,6 +1,12 @@
 import {TodoListType} from "../../App";
 import {v1} from "uuid";
-import {addTodolistAC, changeFilterInTodolistAC, removeTodolistAC, todolistReducer} from "./todolistReducer";
+import {
+  addTodolistAC,
+  changeFilterInTodolistAC,
+  changeTitleInTodolistAC,
+  removeTodolistAC,
+  todolistReducer
+} from "./todolistReducer";
 
 let startState: Array<TodoListType> = []
 let todoListID_1: string
@@ -35,5 +41,9 @@ test('remove Todolist', () => {
 test('change filter in Todolist', () => {
   let endState = todolistReducer(startState , changeFilterInTodolistAC(todoListID_2, 'active'))
   expect(endState[1].filter).toBe('active')
-
+})
+test('change title in Todolist', () => {
+  let endState = todolistReducer(startState , changeTitleInTodolistAC(todoListID_1, 'NBA'))
+  expect(endState[0].id).toBe(todoListID_1)
+  expect(endState[0].title).toBe('NBA')
 })
