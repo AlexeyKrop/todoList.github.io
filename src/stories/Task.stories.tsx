122e100c2e2b2db9725698519a84c1da2f822dbf
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-17 10:55:58 +0300

    add TaskIsNotDoneStories

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,12 +1,28 @@
-import {AddItemForm} from "../Components/AddItemForm";
-import {ComponentMeta} from "@storybook/react";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {Task} from "../Components/Task";
+import React from "react";
+import {action} from "@storybook/addon-actions";
+import {AddItemFormStories} from "./AddItemForm.stories";
+import {TaskType} from "../App";
 
 export default {
-  title: 'Todolist/AddItemForm',
-  component: AddItemForm,
-  argTypes: {
-    addItem: {
-      description: 'button click in form'
-    },
-  },
-} as ComponentMeta<typeof AddItemForm>
+  title: 'Todolist/Task',
+  component: Task,
+} as ComponentMeta<typeof Task>
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+export const TaskIsDoneStories = Template.bind({});
+TaskIsDoneStories.args = {
+  tasks: [{id: 'dsfds', isDone: true, title: 'JS'}],
+  todoListId: 'dfgdfg',
+  removeTask: action('removeTask'),
+  onChangeStatusInputHandler: action('onChangeStatusInputHandler'),
+  changeTaskHandler: action('changeTaskHandler')
+};
+export const TaskIsNotDoneStories = Template.bind({});
+TaskIsNotDoneStories.args = {
+  tasks: [{id: 'dsfds', isDone: false, title: 'JS'}],
+  todoListId: 'dfgdfg',
+  removeTask: action('removeTask'),
+  onChangeStatusInputHandler: action('onChangeStatusInputHandler'),
+  changeTaskHandler: action('changeTaskHandler')
+};
