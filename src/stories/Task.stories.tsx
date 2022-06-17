0a45b4449ad3c1d99b25c3853f3b812e030a5e9f
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-17 11:01:08 +0300

    add args in TaskStories

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -6,21 +6,19 @@ import {action} from "@storybook/addon-actions";
 export default {
   title: 'Todolist/Task',
   component: Task,
+  args:{
+    todoListId: 'dfgdfg',
+    removeTask: action('removeTask'),
+    onChangeStatusInputHandler: action('onChangeStatusInputHandler'),
+    changeTaskHandler: action('changeTaskHandler')
+  }
 } as ComponentMeta<typeof Task>
 const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 export const TaskIsDoneStories = Template.bind({});
 TaskIsDoneStories.args = {
   tasks: [{id: 'dsfds', isDone: true, title: 'JS'}],
-  todoListId: 'dfgdfg',
-  removeTask: action('removeTask'),
-  onChangeStatusInputHandler: action('onChangeStatusInputHandler'),
-  changeTaskHandler: action('changeTaskHandler')
 };
 export const TaskIsNotDoneStories = Template.bind({});
 TaskIsNotDoneStories.args = {
   tasks: [{id: 'dsfds', isDone: false, title: 'JS'}],
-  todoListId: 'dfgdfg',
-  removeTask: action('removeTask'),
-  onChangeStatusInputHandler: action('onChangeStatusInputHandler'),
-  changeTaskHandler: action('changeTaskHandler')
 };
